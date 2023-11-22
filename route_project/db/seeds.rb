6b86f2019902c86fcac766b756128b870e790f16
Shaun Jhingoor <Jhingoor1945@gmail.com>
2023-11-22 15:27:18 -0500

    Finish controller 1

--- route_project/db/seeds.rb
+++ route_project/db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+ User.create(name: 'shaun', email: "shaun@gamil.com")
+ User.create(name: 'Ishan', email: 'Ishan@gmail.com')
