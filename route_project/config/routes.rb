1ac67c9f7fb5938a8694b9d09973bdec61d94195
Shaun Jhingoor <Jhingoor1945@gmail.com>
2023-11-22 14:34:35 -0500

    Finish part 1

---
+++ route_project/config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+  get   '/users', to: 'users#index', as: :users
+  post   '/users', to: 'users#create'
+  get  '/users/new', to: 'users#new', as: :new_user
+  get   '/users/:id/edit', to: 'users#edit', as: :edit_user
+  get   '/users/:id', to: 'users#show', as: :user
+  patch  '/users/:id', to: 'users#update'
+  put    '/users/:id', to:  'users#update'
+  delete '/users/:id', to:  'users#destroy'
+  # resources :users
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
